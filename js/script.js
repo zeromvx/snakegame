00748ab4bb3bb43666aa908b82aef5b931b2d0b4
zeromvx <zeromvx@gmail.com>
2020-07-17 16:18:37 +1000

    Changed logic of positioning, spawn apples, drawing snake

--- js/script.js
+++ js/script.js
@@ -1,38 +1,45 @@
 const canv = document.getElementById("canvas"); // canvas
 const ctx = canv.getContext("2d"); // 2d context
-const scoreBox = document.querySelector(".score");
+const scoreBox = document.querySelector(".score");	// area for outputing score
 
-const appleImg = new Image(32, 32);
-appleImg.src = "img/apple.png";
-
-let boxSize = 32; // width & height 1 cell
 let score = 0;
 
 let xv = 0; // x motion
 let yv = 0; // y motion
 let cooldown = false; // cooldown for keys
 
-let apple = {
-	x: Math.floor(Math.random() * 19 + 1) * boxSize,
-	y: Math.floor(Math.random() * 18 + 2) * boxSize,
-}; // apple obj, at 1 time, apples generating here ,then calling updateApple coords function
+let cellSize = 20; // 1 cell - 20px
+let cellsCount = 20; // 400px - 20 * 20 cells
 
-let snake = []; // snake objects array
+// apple obj, at 1 time, apples generating here ,then calling updateApple coords function
+let apple = {
+	x: Math.floor(Math.random() * cellsCount),
+	y: Math.floor(Math.random() * cellsCount),
+};
+
+// snake objects array
+let snake = [];
 snake[0] = {
-	x: Math.floor(canv.clientWidth / 2),
-	y: Math.floor(canv.clientHeight / 2),
+	x: 9,
+	y: 9,
 };
 
 document.addEventListener("keydown", changeDirection);
-let game = setInterval(drawGame, 100); // 30 fps
+let game = setInterval(drawGame, 1000 / 8); // 30 fps
 
 function drawGame() {
 	// canvas settings
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, 0, canv.clientWidth, canv.clientHeight);
 
-	// drawing apple image
-	ctx.drawImage(appleImg, apple.x, apple.y);
+	// drawing apple
+	ctx.fillStyle = "red";
+	ctx.fillRect(
+		apple.x * cellsCount,
+		apple.y * cellsCount,
+		cellSize - 2,
+		cellSize - 2
+	);
 
 	updateSnakeCoords();
 }
@@ -45,27 +52,37 @@ function updateSnakeCoords() {
 	let snakeX = snake[0].x;
 	let snakeY = snake[0].y;
 
+	// snake motions
 	snakeX += xv;
 	snakeY += yv;
 
-	// teleports, 32 cuz first string for outputing scores
-	if (snakeX > canv.clientWidth) {
+	// teleports
+	if (snakeX > cellsCount - 1) {
 		snakeX = 0;
 	}
-	if (snakeY > canv.clientHeight) {
+	if (snakeY > cellsCount - 1) {
 		snakeY = 0;
 	}
 	if (snakeX < 0) {
-		snakeX = canv.clientWidth;
+		snakeX = cellsCount - 1;
 	}
 	if (snakeY < 0) {
-		snakeY = canv.clientHeight;
+		snakeY = cellsCount - 1;
 	}
 
 	// drawing snake
+	// snake coords is 0 - 19 for x and y axis`s.
+	// we have area 400x400. every update we get new snake`s coords, that changed on 1
+	// for drawing snake we snake`s coords multiply on area`s cells count
+	// snake`s cell size is cellSize - 2 needs for some distance between cells
 	for (let i = 0; i < snake.length; i++) {
 		ctx.fillStyle = "grey";
-		ctx.fillRect(snake[i].x, snake[i].y, boxSize, boxSize);
+		ctx.fillRect(
+			snake[i].x * cellsCount,
+			snake[i].y * cellsCount,
+			cellSize - 2,
+			cellSize - 2
+		);
 	}
 
 	// check for apple eating
@@ -89,8 +106,8 @@ function updateSnakeCoords() {
 
 // updating apple coords when it called
 function updateAppleCoords() {
-	apple.x = Math.floor(Math.random() * 19 + 1) * boxSize;
-	apple.y = Math.floor(Math.random() * 18 + 2) * boxSize;
+	apple.x = Math.floor(Math.random() * cellsCount);
+	apple.y = Math.floor(Math.random() * cellsCount);
 }
 
 function checkTailEat(head, body) {
@@ -107,23 +124,23 @@ function changeDirection(e) {
 	}
 	// left arrow
 	if (e.keyCode == 37 && !(xv > 0)) {
-		xv = -boxSize;
+		xv = -1;
 		yv = 0;
 	}
 	// top arrow
 	if (e.keyCode == 38 && !(yv > 0)) {
 		xv = 0;
-		yv = -boxSize;
+		yv = -1;
 	}
 	// right arrow
 	if (e.keyCode == 39 && !(xv < 0)) {
-		xv = boxSize;
+		xv = 1;
 		yv = 0;
 	}
 	// down arrow
 	if (e.keyCode == 40 && !(yv < 0)) {
 		xv = 0;
-		yv = boxSize;
+		yv = 1;
 	}
 
 	cooldown = true;
