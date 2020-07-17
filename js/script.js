41d1bf1c9aa49625be323d4ae2cf9a47809501e9
zeromvx <zeromvx@gmail.com>
2020-07-17 10:39:19 +1000

    changed logic

--- js/script.js
+++ js/script.js
@@ -4,16 +4,20 @@ const ctx = canv.getContext("2d"); // 2d context
 const appleImg = new Image(32, 32);
 appleImg.src = "img/apple.png";
 
-let boxSize = 32;
-let dir = "";
+let boxSize = 32; // width & height 1 cell
 let score = 0;
+
+
+let xv = 0; // x motion
+let yv = 0; // y motion
+let cooldown = false; // cooldown for keys
 
 let apple = {
 	x: Math.floor(Math.random() * 19 + 1) * boxSize,
-	y: Math.floor(Math.random() * 19 + 1) * boxSize,
-};
+	y: Math.floor(Math.random() * 18 + 2) * boxSize,
+}; // apple obj, at 1 time, apples generating here ,then calling updateApple coords function
 
-let snake = [];
+let snake = []; // snake objects array
 snake[0] = {
 	x: Math.floor(canv.clientWidth / 2),
 	y: Math.floor(canv.clientHeight / 2),
@@ -25,33 +29,48 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function drawGame() {
+	// canvas settings
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, 0, canv.clientWidth, canv.clientHeight);
 
-	ctx.fillStyle = "black";
+	// drawing apple image
 	ctx.drawImage(appleImg, apple.x, apple.y);
 
-	for (let i = 0; i < snake.length; i++) {
-		ctx.fillStyle = "grey";
-		ctx.fillRect(snake[i].x, snake[i].y, boxSize, boxSize);
-	}
 	updateSnakeCoords();
 }
 
-function updateAppleCoords() {
-	apple.x = Math.floor(Math.random() * 19 + 1) * boxSize;
-	apple.y = Math.floor(Math.random() * 19 + 1) * boxSize;
-}
-
+// function for updating snake coords. 
+// if apple not eaten, delete elem of snake array from end and add new element to start
+// if apple eaten, don`t delete elem and add new elem to start
+// snake moves cuz we delete and add end and
 function updateSnakeCoords() {
 	let snakeX = snake[0].x;
 	let snakeY = snake[0].y;
 
-	if (snakeX > canv.clientWidth) snakeX = 0;
-	if (snakeY > canv.clientHeight) snakeY = 0;
-	if (snakeX < 0) snakeX = canv.clientWidth;
-	if (snakeY < 0) snakeY = canv.clientHeight;
+	snakeX += xv;
+	snakeY += yv;
 
+	// teleports, 32 cuz first string for outputing scores
+	if (snakeX > canv.clientWidth) {
+		snakeX = 0;
+	}
+	if (snakeY > canv.clientHeight) {
+		snakeY = 32;
+	}
+	if (snakeX < 0) {
+		snakeX = canv.clientWidth;
+	}
+	if (snakeY < 32) {
+		snakeY = canv.clientHeight;
+	}
+
+	// drawing snake
+	for (let i = 0; i < snake.length; i++) {
+		ctx.fillStyle = "grey";
+		ctx.fillRect(snake[i].x, snake[i].y, boxSize, boxSize);
+	}
+
+	// check for apple eating
 	if (snakeX == apple.x && snakeY == apple.y) {
 		score++;
 		updateAppleCoords();
@@ -59,27 +78,58 @@ function updateSnakeCoords() {
 		snake.pop();
 	}
 
-	if (dir === "left") snakeX -= boxSize;
-	if (dir === "right") snakeX += boxSize;
-	if (dir === "up") snakeY -= boxSize;
-	if (dir === "down") snakeY += boxSize;
-
 	let newSnakeHead = {
 		x: snakeX,
 		y: snakeY,
 	};
 
+	checkTailEat(newSnakeHead, snake);
+
 	snake.unshift(newSnakeHead);
 }
 
-function changeDirection(e) {
-	if (e.keyCode == 37 && dir != "right") {
-		dir = "left";
-	} else if (e.keyCode == 38 && dir != "down") {
-		dir = "up";
-	} else if (e.keyCode == 39 && dir != "left") {
-		dir = "right";
-	} else if (e.keyCode == 40 && dir != "up") {
-		dir = "down";
+// updating apple coords when it called
+function updateAppleCoords() {
+	apple.x = Math.floor(Math.random() * 19 + 1) * boxSize;
+	apple.y = Math.floor(Math.random() * 18 + 2) * boxSize;
+}
+
+function checkTailEat(head, body) {
+	for (let i = 0; i < body.length; i++) {
+		if ( head.x == body[i].x && head.y == body[i].y) {
+			clearInterval(drawGame);
+		}
 	}
+}
+
+function changeDirection(e) {
+	if (cooldown) {
+		return false;
+	}
+	// left arrow
+	if (e.keyCode == 37 && !(xv > 0)) {
+		xv = -boxSize;
+		yv = 0;
+	}
+	// top arrow
+	if (e.keyCode == 38 && !(yv > 0)) {
+		xv = 0;
+		yv = -boxSize;
+	}
+	// right arrow
+	if (e.keyCode == 39 && !(xv < 0)) {
+		xv = boxSize;
+		yv = 0;
+	}
+	// down arrow
+	if (e.keyCode == 40 && !(yv < 0)) {
+		xv = 0;
+		yv = boxSize;
+	}
+
+	cooldown = true;
+
+	setTimeout(function () {
+		cooldown = false;
+	}, 100);
 }
